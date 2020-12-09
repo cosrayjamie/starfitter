af8cd0ad0dde40e2e770199f81ffdbf241b91e4e
cosrayjamie <jamie.zvirzdin@gmail.com>
2020-12-09 10:56:40 -0500

    OneDrive Problem, fixed. Added OpenCV test folder.

--- boot_TAx4.sh
+++ boot_TAx4.sh
@@ -1,4 +1,4 @@
-nice ./new_starfitter 3 mirror_geometry.dat results/md_m25.stars.txt 1>> mirror_geometry.nparm.dat 2>> rejects.err
+nice ./new_starfitter 3 mirror_geometry.dat results/md_m25.stars.txt 1> mirror_geometry.nparm.dat 2> rejects.err
 nice ./new_starfitter 7 mirror_geometry.dat results/md_m25.stars.txt 1>> mirror_geometry.nparm.dat 2>> rejects.err
 nice ./new_starfitter 11 mirror_geometry.dat results/md_m25.stars.txt 1>> mirror_geometry.nparm.dat 2>> rejects.err
 nice ./new_starfitter 15 mirror_geometry.dat results/md_m25.stars.txt 1>> mirror_geometry.nparm.dat 2>> rejects.err
